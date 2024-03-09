86a6c2b5e581daaae6ba5c48bf8feccbfb090719
Luana Portezan <luanaportezan@gmail.com>
2024-03-09 11:30:12 -0300

    feat: random password

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+import { nanoid } from 'nanoid';
+
+const passwordButtonElement = document.querySelector('button');
+const displayPasswordElement = document.querySelector('h2');
+
+passwordButtonElement.addEventListener('click', () => {
+    const randomPassword = nanoid();
+    displayPasswordElement.innerHTML = randomPassword;
+});
