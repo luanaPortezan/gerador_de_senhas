77873b4cdf7cbbe87581b6986a48a3cb39c012ff
Luana Portezan <luanaportezan@gmail.com>
2024-04-10 15:02:43 -0300

    feat: clicking on the password tag it will be copied

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import { copy } from 'clipboard-copy';
 import './style.css';
 
 const passwordButtonElement = document.querySelector('button');
@@ -7,4 +8,9 @@ const displayPasswordElement = document.querySelector('h2');
 passwordButtonElement.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordElement.innerHTML = randomPassword;
+});
+
+displayPasswordElement.addEventListener('click', (event) => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
 });
